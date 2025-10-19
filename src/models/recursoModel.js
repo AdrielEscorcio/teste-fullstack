165439e0eccbf56c84b18b07a4bfacc4ae011f8a
Adriel Escorcio <adrielescorcio96@gmail.com>
2025-10-19 12:10:33 -0300

    feat(model) - adicionado as operações de CRUD do db no model

--- src/models/recursoModel.js
+++ src/models/recursoModel.js
@@ -1,3 +1,5 @@
+const {run, all, get} = require('../../db/dbCrud')
+
 function base (tabela) {
 
     const acoes = {
@@ -12,6 +14,12 @@ function base (tabela) {
     function definirValoresChaves(acao,dados) {
         const valores = Object.values(dados).map(v => v)
         const chaves = Object.keys(dados).join(', ')
+
+        if(acao === 'atualizar'){
+            const chavesAtualizar = Object.keys(dados).join(' = ?, ').concat(' = ?')
+            return {valores, chavesAtualizar}
+        }
+        
         if(acao === 'inserir'){
             const reservado = valores.map(() => '?').join(', ')
             return {valores, chaves, reservado};
@@ -21,53 +29,54 @@ function base (tabela) {
     }
     
     const crud = {
-            inserir: (dados) => {
+            inserir: async (dados) => {
                 const {valores, chaves, reservado } = definirValoresChaves(acoes.inserir,dados)
                 const sql = `INSERT INTO ${tabela} (${chaves}) VALUES (${reservado})`
-                console.log(sql, [...valores]);
+                console.log(valores)
+                await run(sql, [...valores])
+                return dados
             },
-            listar: () => {
+            listar: async () => {
                 const sql = `SELECT * FROM ${tabela}`;
                 console.log(sql);
-                return sql;
+                const res = await all(sql)
+                console.log(res)
+                return res; 
             },
-            listarPorID: (id) => {
+            listarPorID: async (id) => {
                 const sql = `SELECT * FROM ${tabela} where id = ?`;
-                console.log(sql,[id]);
-                return sql;
+                const res = await get(sql,[id]);
+                console.log(res)
+                return res;
             },
-            atualizar: (id, dados) => {
-                const {valores, chaves} = definirValoresChaves(acoes.atualizar,dados)
-                const sql = `UPDATE ${tabela} SET ${chaves}  WHERE id = ?`
-                console.log(sql, [...valores, id]);
-                return sql;
+            atualizar: async (id, dados) => {
+                const {valores, chavesAtualizar} = definirValoresChaves(acoes.atualizar,dados)
+                const sql = `UPDATE ${tabela} SET ${chavesAtualizar}  WHERE id = ?`
+                await run(sql, [...valores, id]);
             },
-            deletar: (id) => {
+            deletar: async (id) => {
                 const sql = `DELETE FROM ${tabela} WHERE id = ?`
-                console.log(sql, [id]);
-                return sql;
+                return await run(sql, [id]);
             },
-            listarPorIDs: (recurso, id, extra, idExtra) => {
+            listarPorIDs: async (recurso, id, extra, idExtra) => {
                 const sql = `SELECT 1 FROM ${tabela} WHERE ${recurso}_id = ? AND ${extra}_id = ?;`;
-                console.log(sql, [id, idExtra]);
-                return sql;
+                const res = await get(sql, [id, idExtra]);
+                console.log(res)
+                return res;
             },
-            listarExtraDeRecurso: (id, extra, recurso) => {
+            listarExtraDeRecurso: async (id, extra, recurso) => {
                 const sql = `SELECT * FROM ${extra} AS c INNER JOIN ${tabela} AS pc ON c.id = pc.${extra}_id WHERE pc.${recurso}_id = ?;`;
-                console.log(sql, [id]);
-                return sql;
+                const res = await all(sql, [id]);
+                return res;
             },
-            deletarRecursoExtra: (recurso, id, extra, idExtra) => {
+            deletarRecursoExtra: async (recurso, id, extra, idExtra) => {
                 const sql = `DELETE FROM ${tabela} WHERE ${recurso}_id = ? AND ${extra}_id = ?;`;
-                console.log(sql, [id, idExtra])
-                return sql;
-
+                await run(sql, [id, idExtra])
             }, 
-            criarRecursoExtra: (recurso, id, extra, dados) => {
+            criarRecursoExtra: async (recurso, id, extra, dados) => {
                 const {valores} = definirValoresChaves(acoes.inserir, dados)
                 const sql = `INSERT INTO ${tabela} (${recurso}_id, ${extra}_id) VALUES (?, ?);`
-                console.log(sql, [id, ...valores]) 
-                return sql;
+                await run(sql, [id, ...valores]) 
             }
         }
 
