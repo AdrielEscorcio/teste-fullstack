4ce2de0dd338afd187e2965b10fa68f3b2744cf6
Adriel Escorcio <adrielescorcio96@gmail.com>
2025-10-19 20:44:45 -0300

    feat(nav) - menu de navegação

--- front/src/components/menu.tsx
+++ front/src/components/menu.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+import style from "./menu.module.css"
+import { LuLayoutDashboard, LuUsers, LuFolder } from 'react-icons/lu';
+
 export default function Menu(){
     return (
-        <ul>
-            <li>Dashboard</li>
-            <li>Projetos</li>
-            <li>Colaboradores</li>
+        <ul className={`${style.component}`}>
+            <Link href={"/dashboard"}><li><LuLayoutDashboard /> Dashboard</li></Link>
+            <Link href={"/projetos"}><li><LuUsers /> Projetos</li></Link>
+            <Link href={"/colaboradores"}><li><LuFolder />Colaboradores</li></Link>
         </ul>
     )
 }
+
